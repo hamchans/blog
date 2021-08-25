76de626991640c1346b170ea64b136724a6254a3
Yamada Kohki <kohki.y@Kohki-Mac.local>
2021-08-25 21:42:23 +0900

    Aug 25th 2021

--- javascript/rightSideNavi.js
+++ javascript/rightSideNavi.js
@@ -5,21 +5,21 @@ function rightSide() {
     html += '<li><a href="">東南アジア</a></li>';
     html += '<li><a href="/trip/Taiwan/r1_9_Taiwan.html">台湾</a></li>';
     html += '<li><a href="/trip/France/r1_11_France.html">フランス</a></li>';
-    html += '<li><a href="">北海道</a></li>';
+    html += '<li><a href="/trip/Hokkaido">北海道</a></li>';
     html += '<li><a href="/trip/Hungary/r2_2-3_Hungary.html">ハンガリー</a></li>';
     html += '<li><a href="/trip/Austria/r2_2_Austria.html">オーストリア</a></li>';
     html += '<li><a href="">広島島根鳥取京都</a></li>';
     html += '<li><a href="/trip/Osaka_Nara_Hyogo/r2_11_Osaka_Nara_Hyogo.html">大阪奈良兵庫</a></li>';
     html += '</ul>';
-    html += '<h5 class="profile">プロフィール</h5>';
+    html += '<h5 class="profile"><a href="/profile">プロフィール</a></h5>';
     html += '<p class="profile">';
     html += '名前：はむちゃん<br>';
     html += '出身：埼玉県<br>';
-    html += '高校：東京都某私立高校<br>';
+    html += '高校：創価高校<br>';
     html += '大学：慶應義塾大学<br>';
     html += '学部：理工学部<br>';
     html += '学科：情報工学科<br>';
-    html += '趣味：旅行、野球観戦<br>';
+    html += '趣味：旅行・野球観戦<br>';
     html += '</p>';
     document.write(html);
 }
